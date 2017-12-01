3fd31db83f6bb79d1c6ed13d8a6ba0c8766cae8b
bluessista <inga@bluessista.com>
2017-12-01 22:56:36 +0100

    distance ready - location still missing

--- js/driver.js
+++ js/driver.js
@@ -254,56 +254,18 @@ var clients = [
 // a. distance in meter umwandeln, latitude & longitude -> 'lat' und 'long'
 // b. distanceLength -> distance, position [lat, long, height] in location: {lat: lat, long: long}
 
+// 1.
 
+const clientsLess3000m = clients.filter((client) => (client.distance < 3 || client.distanceLength < 3000) ? true : false);
 
-
-
-//
-// clientsWithMetersLessThan3000 = clients.filter(client => {
-//    return (distance < 3 || distanceLength < 3000) ? true : false
-// })
-//
-// desiredClients = [];
-//
-// clientsWithMetersLessThan3000.forEach(client => {
-//    if (a) {
-//      distanceInMeter();
-//        client.location.latitude
-//      }
-//    else {
-//      renameProperties();
-//      positionArrayToLocationObject(client);
-//    }
-// })
-
-const object = {
-  key : 'value',
-  value : 'value2',
-  nested : {
-    key: 'value3'
-  }
+// 2.
+const locationKeyShortener = (client) => {
+  const location = {
+    lat: client.location.latitude,
+    long: client.location.longitude
+  };
+  return location;
 };
-
-for (let propertyName in object) {
-  if (object.hasOwnProperty(propertyName)) {
-
-  }
-   // value, value2, [key: 'value3}
-}
-
-const object2 = {
-  key: object.value
-};
-
-console.log(object.key) //value
-console.log(object['key']) // value
-console.log(object[object.key]) // object.value object['value'] = value2
-object.nested.key = "value3";
-// value3 mit value
-object.nested.key = 'value';
-object.nested.key = object.key;
-
-
 
 const positionArrayToLocationObject = (client) => {
   const location = {
@@ -313,170 +275,22 @@ const positionArrayToLocationObject = (client) => {
   return location;
 };
 
+var newClients = clientsLess3000m.map((client) => {
 
-
-var renameProperties = (obj, from, to) => {
-  obj[to] = obj[from];
-  delete obj[from];
-};
-newClients = [];
-//alle distance Werte in Meter umwandeln
-const handle = (client) => {
+//2a.
   if(client.hasOwnProperty('distance')) {
     client.distance *= 1000;
+
+    locationKeyShortener(client);
   }
 
-  if(client.distanceLength) {
-
-    clients.forEach((obj) => {
-      renameProperties(obj, 'distanceLength', 'distance')
-    });
-  };
+//2b.
+  if(client.hasOwnProperty('distanceLength')) {
+    client.distance = client.distanceLength;
+    delete client.distanceLength;
+    positionArrayToLocationObject(client);
+  }
   return client;
-}
+});
 
-const result = clients.map(handle);
-
-
-console.log(meters, clients);
-
-//Werte aus distanceLength in distance speichern und distanceLength löschen
-
-//=> warum wird hier die distance der oberen Werte überschrieben?
-//Objekte, die distance < 3000 sind, ausgeben
-
-//wenn im Objekt position steht, dann den ersten und zweiten Wert aus dem Array auslesen und in location object (location.lat und location.long) speichern
-
-//position löschen
-
-
-
-// function change () {
-//     for (i=0; i<clients.length; i++) {
-//         let client = clients[i];
-//         if (client.distance != undefined && client.distance <= 100) {
-//             client.distance = client.distance * 1000;
-//         } else if(client.distanceLength != undefined && client.distanceLength <= 100) {
-//             client.distanceLength = client.distanceLength * 1000
-//         }
-//     }
-// }
-//
-// change();
-//
-// function DistanceUnder3000() {
-//
-//     let underMaxDistance = [];
-//
-//     for (i = 0; i < clients.length; i++) {
-//         let client = clients[i];
-//         distance1 = client.distance;
-//         distance2 = client.distanceLength;
-//         if ((distance1 || distance2) <= 3000) {
-//             underMaxDistance.push(client);
-//         }
-//     }
-//
-//     console.log(underMaxDistance);
-// }
-//
-// DistanceUnder3000();
-//
-// function sameDistanceKeys() {
-//   var renameProperties = (obj, from, to) => {
-//       obj[to] = obj[from];
-//       delete obj[from];
-//     };
-//
-//   if(clients.distanceLength) {
-//       clients.forEach(obj => renameProperties(obj, 'distanceLength', 'distance'));
-//   };
-// };
-//
-// sameDistanceKeys();
-//
-
-// let distanceUnder3000m = (arr) => {
-//     //all distances should be counted in meters
-//   // arr.distance = arr.map((client) => client.distance *= 1000);
-//   // console.log(arr);
-//
-//
-//   if(arr.distanceLength) {
-//     arr.forEach(obj => renameProperties(obj, 'distanceLength', 'distance'));
-//   };
-//   // // var location = (arr) => {
-//   // //   arr.forEach(obj => renameProperties(obj, 'position', 'location'));
-//   // //   // arr.location = arr.location.slice(0,2);
-//   // // };
-//   // // location(clients);
-//   arr = arr.filter((client) => {
-//     if (client.distance <= 100) {
-//       console.log(client.distance *= 1000);
-//         if (client.distance < 3000) {
-//           arr.push(client);
-//         }
-//     }
-//   });
-//   //
-//   // console.log(arr.distance);
-//   console.log(arr);
-// };
-//
-// distanceUnder3000m(clients);
-
-// let positionBeLocation = (client) => {
-//   // var location = {
-//   //   arr.location.lat,
-//   //   arr.location.long
-//   // };
-//   console.log(arr);
-//     //all location values I need are lat and long
-//   if(arr.position) {
-//     arr.location.lat = arr.position[0];
-//     arr.location.long = arr.position[1];
-//     delete arr.position;
-//   }
-//
-//   if(arr.location) {
-//     arr.location.lat = arr.location.latitude;
-//     arr.location.long = arr.location.longitude;
-//     delete arr.location.latitude, arr.location.longitude;
-//   }
-//   return arr.location;
-//   // = {
-//   //   location['lat'],
-//   //   location['long']
-//   // };
-// };
-//
-// console.log(positionBeLocation(clients));
-
-
-
-// var shortDistance2 = clients.filter((client) => {
-//   if(client.distance) {
-//     return (client.distance < 3);
-//   }
-// });
-//
-// var availableClients = [
-//   shortDistance1,
-//   shortDistance2
-// ];
-//
-// console.log(availableClients);
-
-// location = {};
-// location.lat = clients.map((e) => {
-//   console.log(e.location[0]);
-//   return e.location[0];
-// });
-
-//});
-
-// availableClients.distance = shortDistance.map((e) => {
-//   if (e <= 3000) {
-//     return availableClients.distance;
-//   }
-// });
+console.log(newClients);
